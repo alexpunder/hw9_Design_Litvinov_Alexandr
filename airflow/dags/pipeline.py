7be2ed1cb52b06558d26cb890fb999b29834ed25
Alexander <alexpunder@yandex.ru>
2026-05-23 19:08:58 +0300

    init project. add docker compose airflow+pg. add DAG pipeline/

---
+++ airflow/dags/pipeline.py
@@ -0,0 +1,76 @@
+import random
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.empty import EmptyOperator
+
+
+def check_retrain_conditions(**context):
+    """
+    Проверяем три условия:
+    1. Суммарно по кассам > 10 млн чеков
+    2. Accuracy упала < 0.85 (дрифт модели)
+    3. Срок действия модели истекает через 1 час
+    """
+    checks_options = [5_000_000, 8_000_000, 10_500_000, 12_000_000, 15_000_000]
+    total_checks = random.choice(checks_options)
+    condition_1 = total_checks > 10_000_000
+    
+    accuracy_options = [0.92, 0.88, 0.83, 0.79, 0.86]
+    current_accuracy = random.choice(accuracy_options)
+    condition_2 = current_accuracy < 0.85
+    
+    deploy_options = [
+        datetime(2025, 1, 1, 0, 0, 0),
+        datetime(2025, 5, 22, 10, 0, 0),
+        datetime(2025, 5, 23, 0, 0, 0),
+        datetime(2025, 5, 23, 20, 0, 0),
+        datetime(2025, 5, 23, 22, 30, 0),
+    ]
+    model_deploy_time = random.choice(deploy_options)
+    time_since_deploy = datetime.now() - model_deploy_time
+    condition_3 = time_since_deploy.total_seconds() > 23 * 3600
+    
+    if condition_1 or condition_2 or condition_3:
+        return "load_data"
+    else:
+        return "skip"
+
+def load_data():
+    print("Загрузка данных чеков...")
+
+def train_model():
+    print("Переобучение модели прогноза запасов...")
+
+def evaluate_model():
+    print("Оценка новой модели: accuracy, precision, recall...")
+
+def deploy_model():
+    print("Деплой новой модели...")
+
+def skip_task():
+    print("Условия для переобучения не выполнены.")
+
+with DAG(
+    dag_id="ml_continuous_training_retail_stocks",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="@hourly",
+    catchup=False,
+    description="CT для складских запасов"
+) as dag:
+    
+    check_branch = BranchPythonOperator(
+        task_id="check_retrain_conditions",
+        python_callable=check_retrain_conditions
+    )
+    
+    skip = EmptyOperator(task_id="skip")
+    
+    load = PythonOperator(task_id="load_data", python_callable=load_data)
+    train = PythonOperator(task_id="train_model", python_callable=train_model)
+    evaluate = PythonOperator(task_id="evaluate_model", python_callable=evaluate_model)
+    deploy = PythonOperator(task_id="deploy_model", python_callable=deploy_model)
+    
+    check_branch >> [load, skip]
+    load >> train >> evaluate >> deploy
